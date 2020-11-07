22623ff9aa698595fbfcfcdc3a7390f598a4e045
aprillebalsom <aprillebalsom@gmail.com>
2020-11-07 11:12:04 -0500

    inital commit

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+//adding theme support
+function sunhome_theme_support() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post-format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+    add_theme_support( 'html5' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'custom-background' );
+    add_theme_support( 'custom-header' );
+    add_theme_support( 'custom-logo' );
+    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support( 'starter-content' );
+}
+
+add_action( 'after_setup_theme', 'sunhome_theme_support' );
+
+
+//importing stylesheet
+function sunhome_enqueue_styles() {
+    wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/styles.css', [], time(), 'all');
+}
+
+add_action( 'wp_enqueue_scripts', 'sunhome_enqueue_styles' );
+
+?>
